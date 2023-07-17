33d8808ab47fe921e57e8af98ad2fd307bff2ba8
Jakesnake01151 <75321506+Jakesnake01151@users.noreply.github.com>
2023-07-17 17:22:43 -0400

    Add files via upload

---
+++ Dv/fxmanifest.lua
@@ -0,0 +1,15 @@
+-- Delete Vehicle script written by WolfKnight
+-- With credit to Jake
+-- Version 1.0.0
+
+-- Define the FX Server version and game type
+fx_version "bodacious"
+game "gta5"
+
+name "Delete Vehicles"
+description "Deletes vehicles!"
+author "Jake"
+version "1.0.0"
+
+-- Add a client script 
+client_script "client.lua"
